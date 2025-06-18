a386e4f44154953df0e41fcf8df7052c2b3a5566
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-06-18 12:56:06 +0700

    fix: correct import path for Home component from "./Pages/Home" to "./pages/Home"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
